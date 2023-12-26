17b698ec4b377a375972ab456dd47c8ce36bb33a
Javokhir <javohir.murodov.9238@gmail.com>
2023-12-26 11:17:35 +0500

    linking telegram bot user input is send to the telegram bot

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,50 +1,118 @@
 import "./Contact.css";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const initialValue = {
+  name: "",
+  phone: "",
+  text: "",
+};
 const Contact = () => {
   const { t } = useTranslation();
 
+  const [formData, setFormData] = useState(initialValue);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    console.log(formData);
+
+    e.preventDefault();
+
+    const { name, phone, text } = formData;
+    const message = `
+      Mijoz Ismi: ${name}\nTelefon raqami: ${phone}\nXabar: ${text}
+    `;
+
+    const botToken = process.env.REACT_APP_BOT_TOKEN;
+    const chatId = process.env.REACT_APP_CHAT_ID;
+
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+          pase_mode: "html",
+          text: message,
+        }),
+      });
+
+      if (response.ok) {
+        // Handle success (e.g., show a success message);
+        toast.success(t("contact.form.success"));
+        setFormData(initialValue);
+      } else {
+        // Handle error
+        console.error("Failed to send message");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <section className="contact container" id="contact">
-      <h2 className="contact__title text-center">{t('contact.title')}</h2>
+      <ToastContainer />
+      <h2 className="contact__title text-center">{t("contact.title")}</h2>
       <div className="contact__info">
-        <form className="contact__form">
+        <form className="contact__form" onSubmit={handleSubmit}>
           <div className="input-groups">
             <div className="group">
               <label htmlFor="name" className="mb-1">
-                {t('contact.form.input1.label')}
+                {t("contact.form.input1.label")}
               </label>
               <input
-                placeholder={t('contact.form.input1.placeholder')}
+                placeholder={t("contact.form.input1.placeholder")}
                 className="form-control"
                 type="text"
                 id="name"
                 name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
               />
             </div>
             <div className="group">
               <label htmlFor="number" className="mb-1">
-                {t('contact.form.input2.label')}
+                {t("contact.form.input2.label")}
               </label>
               <input
-                placeholder={t('contact.form.input2.placeholder')}
+                placeholder={t("contact.form.input2.placeholder")}
                 className="form-control"
                 type="number"
                 id="number"
-                name="number"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
               />
             </div>
           </div>
           <label htmlFor="floatingTextarea" className="mb-1">
-            {t('contact.form.input3.label')}
+            {t("contact.form.input3.label")}
           </label>
           <textarea
             className="form-control"
-            placeholder={t('contact.form.input3.placeholder')}
+            placeholder={t("contact.form.input3.placeholder")}
             id="floatingTextarea"
+            name="text"
+            value={formData.text}
+            onChange={handleChange}
+            required
           ></textarea>
 
-          <button className="contact-btn">{t('contact.form.button')}</button>
+          <button className="contact-btn">{t("contact.form.button")}</button>
         </form>
         <div className="contact__map">
           <iframe
