a0128e5189f22ed688daa6d74793093a62b3c620
Javokhir <javohir.murodov.9238@gmail.com>
2023-12-28 11:58:17 +0500

    change routing style

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./i18n";
+
+
 function App() {
   return <RouterProvider router={routes} />;
 }
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,47 @@
-import { lazy } from "react";
-import { createBrowserRouter } from "react-router-dom";
+// import { lazy } from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import Root from "./pages/Root";
+// import Home from "./pages/Home";
+
+// const About = lazy(() => import("./pages/About"));
+
+// const routes = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       { path: "about", element: <About /> },
+//     ],
+//   },
+// ]);
+
+// export default routes;
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
 
 const About = lazy(() => import("./pages/About"));
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      { path: "about", element: <About /> },
-    ],
-  },
-]);
 
-export default routes;
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Root />}
+        >
+          <Route index element={<Home />} />
+          <Route path="about" element={<Suspense fallback={<div>Loading...</div>}><About /></Suspense>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
