c9e8042ef9e18508dad2a21043b02276d3f349e5
Javokhir <javohir.murodov.9238@gmail.com>
2023-12-28 11:44:42 +0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
